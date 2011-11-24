a0b7c16689ff674646e1b6f5026f461695759498
Steven Kang <s.steven.kang@gmail.com>
2011-11-23 23:09:39 -0500

    wrote ContactsDAO, but not tested.  I'll write a simple test to see if it works tomorrow.  Also, if I have time, i'll write the login page

--- WebappHelp/src/webapp/help/beans/ContactBean.java
+++ WebappHelp/src/webapp/help/beans/ContactBean.java
@@ -1,30 +1,79 @@
 package webapp.help.beans;
-import com.google.appengine.api.datastore.Key;
-import com.google.apphosting.api.DatastorePb.GetResponse.Entity;
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.users.User;
 
-public class ContactBean extends Entity{
-	private static String kind = "Contact";
+public class ContactBean{
+	public static String kind = "Contact";
+	public static String PROPERTY_OWNER = "owner";
+	public static String PROPERTY_NAME = "name";
+	public static String PROPERTY_PHONE = "phone";
+	public static String PROPERTY_EMAIL = "email";
+	public static String PROPERTY_MSG = "msg";
+	public static String PROPERTY_CAT = "category";
 	
-	private Key categoryKey;
+	private Key key;
+	private User owner;
 	private String name;
 	private String phone;
 	private String email;
 	private String message;
+	private Category category;
+	private Entity entity;
 	
-	public ContactBean(Key key, String name, String phone, String email, String message ){
+	public ContactBean(User owner, String name, String phone, String email, Category category, String message){
+		initialize(KeyFactory.createKey(kind, email),
+					owner, name, phone, email, category, message);
 	}
 	
-	public Key getKey(){return categoryKey;}
+	public ContactBean(Key key, User owner, String name, String phone, String email, Category category, String message ){
+		initialize(key, owner, name, phone, email, category, message);
+	}
+	
+	private void initialize(Key key, User owner, String name, String phone, String email, Category category, String message ){
+		this.key = key;
+		this.owner = owner;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.message = message;
+		this.category = category;
+	}
+	
+	public ContactBean(Entity entity){
+		owner = (User) entity.getProperty(PROPERTY_OWNER);
+		name = (String) entity.getProperty(PROPERTY_NAME);
+		phone = (String) entity.getProperty(PROPERTY_PHONE);
+		email = (String) entity.getProperty(PROPERTY_EMAIL);
+		message = (String) entity.getProperty(PROPERTY_MSG);
+		category = (Category) entity.getProperty(PROPERTY_CAT);
+		key = entity.getKey();
+		this.entity = entity;
+	}
+	
+	public Key getKey(){return key;}
 	public String getName(){return name;}
+	public String getPhone(){return phone;}
 	public String getEmail(){return email;}
 	public String getMessage(){return message;}
+	public Category getCategory(){return category;}
+	
+	public Entity getEntity(){
+		if(entity == null){
+			entity = new Entity(key);
+			entity.setProperty(PROPERTY_OWNER,owner);
+			entity.setProperty(PROPERTY_NAME,name);
+			entity.setProperty(PROPERTY_PHONE,phone);
+			entity.setProperty(PROPERTY_EMAIL,email);
+			entity.setProperty(PROPERTY_MSG,message);	
+			entity.setProperty(PROPERTY_CAT,category);
+		}
+		
+		return entity;
+	}
 	
 	public static boolean Validate(ContactBean bean){
 		// TODO
 		return false;
 	}
-	
-// overrideen methods
-	
-	public String getKind(){return kind;}
 }
--- WebappHelp/src/webapp/help/dao/ContactsDAO.java
+++ WebappHelp/src/webapp/help/dao/ContactsDAO.java
@@ -1,16 +1,21 @@
 package webapp.help.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 import webapp.help.beans.ContactBean;
+import webapp.help.utility.Category;
 
 public class ContactsDAO {
 	private DatastoreService datastore;
@@ -22,20 +27,40 @@ public class ContactsDAO {
 		datastore = DatastoreServiceFactory.getDatastoreService();
         user = userService.getCurrentUser();
         
-        //datastore.put(bean);
+        datastore.put(bean.getEntity());
         
         return false;
 	}
 
 	public boolean deleteContact(Key contactKey){
-		return false;
+		datastore = DatastoreServiceFactory.getDatastoreService();
+		datastore.delete(contactKey);
+		return true;
 	}
 	
-	public boolean updateContacts(Key contactKey, ContactBean bean){
-		return false;
+	public boolean updateContacts(ContactBean bean){
+		datastore = DatastoreServiceFactory.getDatastoreService();
+		datastore.put(bean.getEntity());
+		return true;
 	}
 	
-	public List<ContactBean> getContacts(Key categoryKey){
+	public List<ContactBean> getContacts(Category category){
+		ArrayList<ContactBean> list = new ArrayList<ContactBean>();
+		userService = UserServiceFactory.getUserService();
+		datastore = DatastoreServiceFactory.getDatastoreService();
+		
+		Query query = new Query(ContactBean.kind).
+						addFilter(	ContactBean.PROPERTY_CAT, 
+									Query.FilterOperator.EQUAL,
+									category);
+	    for(Entity e : datastore.prepare(query).asIterable()){
+	    	list.add(new ContactBean(e));
+	    }
+		
+		return list;
+	}
+	
+	private Entity searchContacts(){
 		return null;
 	}
 }
