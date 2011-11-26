6f10cd93c53439ed946f77fec2cf8c4d9eab4957
Chao Wang <cw1@andrew.cmu.edu>
2011-11-26 16:33:57 -0500

    add view

---
+++ view/data/sitemap.js
@@ -0,0 +1,16 @@
+﻿var sitemap = 
+{
+"rootNodes":[
+{
+"pageName":"Home",
+"type":"Wireframe",
+"url":"Home.html",
+"children":[
+{
+"pageName":"general",
+"type":"Wireframe",
+"url":"general.html"},
+{
+"pageName":"edit",
+"type":"Wireframe",
+"url":"edit.html"}]}]};
